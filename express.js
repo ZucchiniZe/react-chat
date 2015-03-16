749106395f8e6c379d4f28dd90f4bd372fd7015d
ZucchiniZe <me@alexb.ninja>
2015-03-16 11:16:38 -0700

    setup express

--- express.js
+++ express.js
@@ -1,11 +1,28 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var webpack = require('webpack');
+var WebpackDevMiddleware = require('webpack-dev-middleware');
+var config = require('./webpack.config');
 
-server.listen(process.env['PORT'])
+server.listen(process.env.PORT)
 
 app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html')
 
-app.get('*', (req, res) => {
+app.use(WebpackDevMiddleware(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true
+}))
+
+app.get('*', function(req, res) {
   res.render('index')
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'eval',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
     './scripts/index'
   ],
