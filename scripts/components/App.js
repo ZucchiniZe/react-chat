e37c3e7c426810308401a57b54e4d7918a03542a
David <dleavenworth.ep@mysummitps.org>
2015-03-17 13:24:10 -0400

    For saving purposes

--- scripts/components/App.js
+++ scripts/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Messages from './Messages';
+import Header from './Header'; 
 
 export default class App extends React.Component {
   render() {
     return (
       <div className='app'>
+        <Header/>
         <Messages/>
       </div>
     );
---
+++ scripts/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="top"></div>
+    );
+  }
+}
+
+export default Header;
